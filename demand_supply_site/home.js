cdefc2c453f36ec604cc17c411e44bef6a17a2e3
Simon Solomon Galadima <112211938+ssginfotech@users.noreply.github.com>
2023-01-05 12:23:17 +0100

    Update home.js
    home js commit

---
+++ demand_supply_site/home.js
@@ -0,0 +1,8 @@
+document.querySelector('form').addEventListener('submit', function(event) {
+  event.preventDefault();
+
+  var demand = document.querySelector('#demand').value;
+  var supply = document.querySelector('#supply').value;
+
+  // Process demand and supply data and update chart
+});
